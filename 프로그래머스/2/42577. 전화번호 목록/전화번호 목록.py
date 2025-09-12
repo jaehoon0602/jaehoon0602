2a79c990a65849a89a2e5feebbbd2f2f2a5c4769
jaehoon <145235699+jaehoon0602@users.noreply.github.com>
2025-09-13 07:43:01 +0900

    [level 2] Title: 전화번호 목록, Time: 102.94 ms, Memory: 29.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,9 @@
+def solution(phone_book):
+    # 전화번호부를 정렬
+    phone_book.sort()
+    
+    # 인접한 번호끼리만 비교
+    for i in range(len(phone_book) - 1):
+        if phone_book[i+1].startswith(phone_book[i]):
+            return False
+    return True
